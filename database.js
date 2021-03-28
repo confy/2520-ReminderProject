c79ce6063abb9c80c5aa63c4c61f2be78d81270f
Adrian <confuzedfx@gmail.com>
2021-03-28 16:50:44 -0700

    Change database to list of objects

--- database.js
+++ database.js
@@ -1,20 +1,19 @@
-let Database = {
-    cindy: {
+let Database = [{
         id: 1,
         email: "cindy.smith@gmail.com",
         password: "12345678",
         reminders: [
-            { id: 1, title: "abc", description: "abcabc", completed: false }, 
+            { id: 1, title: "abc", description: "abcabc", completed: false },
             { id: 2, title: "123", description: "potato", completed: true },
             { id: 3, title: "zyx", description: "123456789", completed: true }
         ]
     },
-    alex: {
+    {
         id: 2,
         email: "alex.johnson@gmail.com",
         password: "12345678",
         reminders: [{ id: 2, title: "haha", description: "uh oh *****, funny", completed: false }]
     }
-}
+]
 
 module.exports = Database;
