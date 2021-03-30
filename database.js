71d95d73df601f20e544e3d7a0198c794068cc19
AnUnawarewolf <royal.victor.leopoldino@gmail.com>
2021-03-29 22:50:18 -0300

    Modify database to have friend information

--- database.js
+++ database.js
@@ -6,13 +6,17 @@ let Database = [{
             { id: 1, title: "abc", description: "abcabc", completed: false },
             { id: 2, title: "123", description: "potato", completed: true },
             { id: 3, title: "zyx", description: "123456789", completed: true }
-        ]
+        ],
+        friends: [],
     },
     {
         id: 2,
         email: "alex.johnson@gmail.com",
         password: "12345678",
-        reminders: [{ id: 2, title: "haha", description: "uh oh *****, funny", completed: false }]
+        reminders: [
+          { id: 2, title: "haha", description: "uh oh *****, funny", completed: false }
+        ],
+        friends: [],
     }
 ]
 
