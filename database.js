f3d58aa3e96e592d8dee6dba4b8a7eeb13d2fec5
Adrian <confuzedfx@gmail.com>
2021-03-30 13:29:06 -0700

    dummy subtask code

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ let Database = [{
         email: "cindy.smith@gmail.com",
         password: "12345678",
         reminders: [
-            { id: 1, title: "abc", description: "abcabc", completed: false },
+            { id: 1, title: "Important", description: "Laugh and Cry", completed: false, subtasks: [{id: 1, title:"Laugh", completed: false}, {id: 2, title:"Cry", completed: true}] },
             { id: 2, title: "123", description: "potato", completed: true },
             { id: 3, title: "zyx", description: "123456789", completed: true }
         ],
